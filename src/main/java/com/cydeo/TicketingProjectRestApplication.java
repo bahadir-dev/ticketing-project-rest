12ae37541ce96f41a40915c4ba8cec4c5ed265de
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-06-27 12:37:59 +0200

    Reformats code

--- src/main/java/com/cydeo/TicketingProjectRestApplication.java
+++ src/main/java/com/cydeo/TicketingProjectRestApplication.java
@@ -8,11 +8,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class TicketingProjectOrmApplication {
+public class TicketingProjectRestApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(TicketingProjectOrmApplication.class, args);
+		SpringApplication.run(TicketingProjectRestApplication.class, args);
 	}
 
 	@Bean
